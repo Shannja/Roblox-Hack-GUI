9ab9e7c7f37e855aeaaaa972d16bc4c8d6f1a985
Shannja <103908889+Shannja@users.noreply.github.com>
2023-11-03 21:54:54 -0400

    Made changes to loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+local placeId = game.PlaceId
+local BladeBall = 13772394625
+if placeId == BladeBall then
+    loadstring(game:HttpGet("https://github.com/Shannja/Roblox-Hack-GUI/raw/master/BladeBall/main.lua"))()
+end
